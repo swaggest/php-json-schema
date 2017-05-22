bc60d11efed73e9259c9e01c0f6e57159917e168
Viacheslav Poturaev <vearutop@gmail.com>
2017-05-22 22:23:02 +0700

    swagger test

--- tests/src/PHPUnit/Swagger/SwaggerTest.php
+++ tests/src/PHPUnit/Swagger/SwaggerTest.php
@@ -6,6 +6,7 @@ namespace Swaggest\JsonSchema\Tests\PHPUnit\Swagger;
 use Swaggest\JsonSchema\JsonSchema;
 use Swaggest\JsonSchema\ProcessingOptions;
 use Swaggest\JsonSchema\RemoteRef\Preloaded;
+use Swaggest\JsonSchema\SwaggerSchema\SwaggerSchema;
 
 class SwaggerTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,21 +14,19 @@ class SwaggerTest extends \PHPUnit_Framework_TestCase
     {
         $schemaData = json_decode(file_get_contents(__DIR__ . '/../../../../spec/swagger-schema.json'));
 
-
         $refProvider = new Preloaded();
         $refProvider->setSchemaData('http://swagger.io/v2/schema.json', $schemaData);
 
         $options = new ProcessingOptions();
         $options->setRemoteRefProvider($refProvider);
 
-        $schema = JsonSchema::importToSchema($schemaData, $options);
-        $ts = $schema->definitions['header']->properties['maximum'];
-        $max = $schema->definitions['maximum'];
-        $this->assertSame('number', $ts->type);
-
-
         $swaggerData = json_decode(file_get_contents(__DIR__ . '/../../../../spec/petstore-swagger.json'));
-        $petstore = $schema->import($swaggerData);
+        $petStore = SwaggerSchema::create()->import($swaggerData);
+
+        $this->assertSame(
+            '/pet:/pet/findByStatus:/pet/findByTags:/pet/{petId}:/pet/{petId}/uploadImage:/store/inventory:/store/order:/store/order/{orderId}:/user:/user/createWithArray:/user/createWithList:/user/login:/user/logout:/user/{username}',
+            implode(':', array_keys($petStore->paths->toArray()))
+        );
     }
 
 }
