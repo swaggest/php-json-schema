d332cc1af47c83b785fe60429dc8a4123a67f163
Pavel Filípek <filipek@proclient.cz>
2019-08-29 13:58:14 +0200

    repair BasicFetcher - $ref for https source

--- src/RemoteRef/BasicFetcher.php
+++ src/RemoteRef/BasicFetcher.php
@@ -1,16 +1,23 @@
 <?php
-
-namespace Swaggest\JsonSchema\RemoteRef;
-
-use Swaggest\JsonSchema\RemoteRefProvider;
-
-class BasicFetcher implements RemoteRefProvider
-{
+  
+  namespace Swaggest\JsonSchema\RemoteRef;
+  
+  use Swaggest\JsonSchema\RemoteRefProvider;
+  
+  class BasicFetcher implements RemoteRefProvider
+  {
     public function getSchemaData($url)
     {
-        if ($data = file_get_contents(rawurldecode($url))) {
-            return json_decode($data);
-        }
-        return false;
+      $arrContextOptions = [
+        "ssl" => [
+          "verify_peer"      => FALSE,
+          "verify_peer_name" => FALSE,
+        ],
+      ];
+      
+      if ($data = file_get_contents(rawurldecode($url), FALSE, stream_context_create($arrContextOptions))) {
+        return json_decode($data);
+      }
+      return false;
     }
-}
+  }
